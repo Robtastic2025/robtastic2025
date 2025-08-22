f4fa6da75ee658b4f02d5a6988f485ff03463139
octusfocus <fkeadress@gmail.com>
2025-08-22 15:34:27 +0300

    retry

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -28,7 +28,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Robtastic2025', // Usually your GitHub org/user name.
-  projectName: 'robtastic2025', // Usually your repo name.
+  projectName: 'robtastic2025.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
